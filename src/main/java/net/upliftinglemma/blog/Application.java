e9d7cbd6e1d39ce5f8bdbd9d2c4ffe880416b8f4
Chris Bouchard <chris@upliftinglemma.net>
2015-08-11 17:32:10 -0400

    Add Hibernate validator and create separate Spring config
    We're going to use the Hibernate validator library to validate our model. We've
    created a bean for the validator factory. It looks like Spring also has some
    validation, but the docs talk about property editors and horrible things like
    that. Hibernate sounds pretty simple.
    
    We've also split out the configuration into its own class, which is imported by
    the application.

--- src/main/java/net/upliftinglemma/blog/Application.java
+++ src/main/java/net/upliftinglemma/blog/Application.java
@@ -1,27 +1,13 @@
 package net.upliftinglemma.blog;
 
-import javax.persistence.EntityManagerFactory;
-import javax.sql.DataSource;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.web.SpringBootServletInitializer;
-import org.springframework.context.annotation.Bean;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.hateoas.config.EnableEntityLinks;
-import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
-import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
-import org.springframework.orm.jpa.JpaTransactionManager;
-import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
-import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
-import org.springframework.transaction.PlatformTransactionManager;
-import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.context.annotation.Import;
 
-@EnableEntityLinks
-@EnableJpaRepositories
-@EnableTransactionManagement
 @SpringBootApplication
+@Import(SpringConfig.class)
 public class Application extends SpringBootServletInitializer {
 
     public static void main(String... args) {
@@ -31,34 +17,6 @@ public class Application extends SpringBootServletInitializer {
     @Override
     protected SpringApplicationBuilder configure(final SpringApplicationBuilder builder) {
         return builder.sources(Application.class);
-    }
-    
-    @Bean
-    public DataSource dataSource() {
-        final EmbeddedDatabaseBuilder builder = new EmbeddedDatabaseBuilder();
-        return builder.setType(EmbeddedDatabaseType.HSQL).build();
-    }
-    
-    @Bean
-    public EntityManagerFactory entityManagerFactory() {
-        final HibernateJpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
-        vendorAdapter.setGenerateDdl(true);
-
-        final LocalContainerEntityManagerFactoryBean factory = new LocalContainerEntityManagerFactoryBean();
-        factory.setJpaVendorAdapter(vendorAdapter);
-        factory.setPackagesToScan("net.upliftinglemma.blog.model");
-        factory.setDataSource(dataSource());
-        factory.afterPropertiesSet();
-
-        return factory.getObject();
-    }
-    
-    @Bean
-    public PlatformTransactionManager transactionManager() {
-
-      final JpaTransactionManager txManager = new JpaTransactionManager();
-      txManager.setEntityManagerFactory(entityManagerFactory());
-      return txManager;
     }
 
 }
